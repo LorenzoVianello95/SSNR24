aff54488ee8542e068a51d9c741c978e0349a957
Lorenzo <lollo.vianello95@gmail.com>
2024-05-30 20:46:59 +0200

    Day 2

--- util.py
+++ util.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy.signal import argrelextrema
+from scipy.interpolate import interp1d
 
 
 def find_local_maxima(array, num_maxima):
@@ -21,3 +22,37 @@ def find_local_maxima(array, num_maxima):
 
     # Return the specified number of local maxima
     return sorted_indices[:num_maxima]
+
+def segment_stride(stance_interpolate_factor, joint, segment_length=100):
+    """
+    Segments gait using stance interpolation factor.
+
+    """
+    segments_alpha = []
+    segments_theta = []
+    start_index = 0
+    completed_gait = False
+
+    for i in range(1, len(stance_interpolate_factor)):
+        if stance_interpolate_factor[i] == 1:
+            completed_gait = True
+        elif completed_gait and stance_interpolate_factor[i] == 0:
+            segments_alpha.append(stance_interpolate_factor[start_index:i])
+            segments_theta.append(joint[start_index:i])
+            start_index = i
+            completed_gait = False
+
+    interpolated_segments_alpha = []
+    interpolated_segments_theta = []
+    for segment_al, segment_th in zip(segments_alpha, segments_theta):
+        if len(segment_al)< 900:
+            x = np.linspace(0, 1, len(segment_al))
+            f = interp1d(x, segment_al, kind='linear')
+            x_new = np.linspace(0, 1, segment_length)
+            interpolated_segments_alpha.append(f(x_new))
+            
+            x = np.linspace(0, 1, len(segment_th))
+            f = interp1d(x, segment_th, kind='linear')
+            interpolated_segments_theta.append(f(x_new))
+
+    return np.array(interpolated_segments_alpha), np.array(interpolated_segments_theta)
